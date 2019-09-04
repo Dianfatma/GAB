34f0ec023e38d10e038e838aa776f37d2506adf3
Emma Paris <eparis@atlassian.com>
2019-09-04 14:23:32 +0700

    first commit

---
+++ public/js/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBLFlgMPxWSE1TcNw0pcbIBWNXdfhTMAxQ",
+    authDomain: "global-arta-borneo.firebaseapp.com",
+    databaseURL: "https://global-arta-borneo.firebaseio.com",
+    projectId: "global-arta-borneo",
+    storageBucket: "",
+    messagingSenderId: "902565496206",
+    appId: "1:902565496206:web:8bf573fb67f91737"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
